70781026e0f154ac439cadd36b70ef33fc753c6b
Logan Jackson <ljacks55@kent.edu>
2025-05-04 23:30:57 -0400

    Added Quasi Carbon Counter
    Made adjustments to the Button to also add the approximate CO2 used to generate the motivational posters

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 import './Button.css';
 
-export default function Button() {
+export default function Button({increment}) {
+  const [count, setCount] = useState(() => {
+    return Number(sessionStorage.getItem("count")) || 0;
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem("count", count);
+  }, [count]);
+
   const handleRefresh = () => {
+    setCount(prev => prev + increment)
     window.location.reload();
   };
 
   return (
     <div className='button'>
-      <button 
-      onClick={handleRefresh}
-    >
-      Motivate Me
-    </button>
-      </div>
+      <button
+        onClick={handleRefresh}
+      >
+        Motivate Me
+      </button>
+      <p>You have wasted {count} grams of CO2</p>
+    </div>
   );
 }
